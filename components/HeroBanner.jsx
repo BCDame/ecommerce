243c00a2f67cb5f591e47c88d22b6afab6d47da3
BC-Learn <badame3132@gmail.com>
2022-11-23 18:53:17 +0530

    hero-banner

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import Link from 'next/link'
+import Link from 'next/link';
+import { urlFor } from '../lib/client';
 
-const HeroBanner = () => {
+const HeroBanner = ({ heroBanner }) => {
   return (
-    <div className='hero-banner-container'>
+    <div className="hero-banner-container">
+    <div>
+      <p className="beats-solo">{heroBanner.smallText}</p>
+      <h3>{heroBanner.MidText}</h3>
+      <h1>{heroBanner.largeText1}</h1>
+      <img src={urlFor(heroBanner.image[0])} alt="headphones" className="hero-banner-image" />
+
       <div>
-        <p className='beats-solo'>SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img alt="headphones" className='hero-banner-image' />
-        <div>
-            <Link href="/product/ID">
-                <button type="button">BUTTON TEXT</button>
-            </Link>
-            <div className='desc'>
-                <h5>Description</h5>
-                <p>DESCRIPTION</p>
-            </div>
+        <Link href={`/product/${heroBanner.product}`}>
+          <button type="button">{heroBanner.buttonText}</button>
+        </Link>
+        <div className="desc">
+          <h5>Description</h5>
+          <p>{heroBanner.desc}</p>
         </div>
       </div>
     </div>
-  )
+  </div>
+)
+
 }
 
 export default HeroBanner
