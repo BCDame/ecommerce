729309fbcdf47f0e881bb8accf0202178a8e0950
BC-Learn <badame3132@gmail.com>
2022-11-23 15:38:50 +0530

    basic-structure

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { Product , HeroBanner , FooterBanner } from '../components/index'
 
 const Home = () => {
   return (
     <>
-    Hero Banner
-    <div>
+    <HeroBanner />
+    <div className='products-heading'>
       <h2>Best Selling Products</h2>
       <h2>Speakers of many variations</h2>
     </div>
+    <div className='products-container'>
+      {['product1','product2'].map((product) => product)}
+    </div>
+    <FooterBanner />
     </>
   )
 }
